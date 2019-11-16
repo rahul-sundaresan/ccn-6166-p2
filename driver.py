90a0ce1013e514be74a6ef1c58a19c71fcd33eb9
Rahul Sundaresan <s.rahul@protonmail.com>
2019-11-16 15:00:10 -0500

    rename variables to be more understandable call GBN sender

--- driver.py
+++ driver.py
@@ -1,4 +1,6 @@
 import sys
+import gbn_sender
+
 
 def read_file_and_return_params(file_name):
     with open(file_name, mode='r') as tcp_deets:
@@ -10,16 +12,15 @@ def read_file_and_return_params(file_name):
         mss = int(tcp_deets.readline().rstrip())
         return tcp_type, seq_bits, window_size, timeout, mss
 
+
 if __name__ == '__main__':
     input_file = sys.argv[1]
-    port = int(sys.argv[2])
+    dst_port = int(sys.argv[2])
     packet_count = int(sys.argv[3])
     tcp_type: str
     tcp_type, seq_bits, window_size, timeout, mss = read_file_and_return_params(input_file)
-    print(tcp_type, seq_bits, window_size, timeout, mss)
     if tcp_type == "GBN":
-        # TODO: code GBN
-        pass
+        gbn_sender.gbn_send(dst_port, packet_count, seq_bits, window_size, timeout, mss)
     else:
         pass
 
