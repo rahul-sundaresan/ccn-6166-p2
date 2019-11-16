eeb6959f80d00f1471b5534d10318a1ab098a791
Rahul Sundaresan <s.rahul@protonmail.com>
2019-11-16 15:40:39 -0500

    change to pickledumps and recv from

--- gbn_receiver.py
+++ gbn_receiver.py
@@ -11,14 +11,14 @@ def gbn_receiver(receiver_host_name, receiver_listening_port):
     expected_seq_num = 1
     while True:
         #  get the raw packet and src ip and port of packet
-        received_raw_packet, client_info = receiver_socket.recv(4096)
+        received_raw_packet, client_info = receiver_socket.recvfrom(4096)
         received_packet = pickle.loads(received_raw_packet)
         received_seq_number, received_checksum, payload, mss, seq_bits = received_packet
         computed_checksum = hashlib.sha256()
-        computed_checksum.update(received_seq_number)
+        computed_checksum.update(pickle.dumps(received_seq_number))
         computed_checksum.update(payload)
-        computed_checksum.update(mss)
-        computed_checksum.update(seq_bits)
+        computed_checksum.update(pickle.dumps(mss))
+        computed_checksum.update(pickle.dumps(seq_bits))
         #  Check if the packet is corrupt
         if received_checksum == computed_checksum.digest():
             if received_seq_number == expected_seq_num:
@@ -26,8 +26,8 @@ def gbn_receiver(receiver_host_name, receiver_listening_port):
                 expected_seq_num = (expected_seq_num + mss) % (2 ** seq_bits - 1)
                 # successfully received mss bytes so expect next mss bytes
                 ack_checksum = hashlib.sha256()
-                ack_checksum.update(expected_seq_num)
-                ack_packet = (expected_seq_num,ack_checksum.digest())
+                ack_checksum.update(pickle.dumps(expected_seq_num))
+                ack_packet = (expected_seq_num, ack_checksum.digest())
                 receiver_socket.sendto(pickle.dumps(ack_packet), client_info)
                 print("ACK Sent:", expected_seq_num)
 
@@ -36,7 +36,7 @@ def gbn_receiver(receiver_host_name, receiver_listening_port):
                 print("Received out of order segment:", received_seq_number)
                 #  send ACK with previous sequence number
                 ack_checksum = hashlib.sha256()
-                ack_checksum.update(expected_seq_num)
+                ack_checksum.update(pickle.dumps(expected_seq_num))
                 ack_packet = (expected_seq_num, ack_checksum.digest())
                 receiver_socket.sendto(pickle.dumps(ack_packet), client_info)
                 print("ACK Sent:", expected_seq_num)
@@ -45,7 +45,7 @@ def gbn_receiver(receiver_host_name, receiver_listening_port):
             print("Received Corrupted segment:", received_seq_number)
             #  send ACK with previous sequence number
             ack_checksum = hashlib.sha256()
-            ack_checksum.update(expected_seq_num)
+            ack_checksum.update(pickle.dumps(expected_seq_num))
             ack_packet = (expected_seq_num, ack_checksum.digest())
             receiver_socket.sendto(pickle.dumps(ack_packet), client_info)
             print("ACK Sent:", expected_seq_num)
